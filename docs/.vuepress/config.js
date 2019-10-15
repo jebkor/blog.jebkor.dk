c2808b1970c80e8027a614f303bc9e582d60d314
jebkor <jesper@jebkor.dk>
2019-10-15 22:11:54 +0200

    Add the initial files
    Prior to this change, there was nothing

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Jebkor Blog',
+  description: 'Ramblings of a delusional frontend developer',
+  themeConfig: {
+      nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Blog', link: '/blog/' }
+      ],
+      sidebar: [
+          '/',
+          '/blog/'
+      ]
+  }
+}
---
+++ docs/blog/2019/october/1st-beginning-of-all.md
@@ -0,0 +1,10 @@
+---
+title: Hello world
+date: 2019-10-15
+description:
+    First,, obligatory post from the beginning of the blog
+---
+
+# First post
+
+/* Your awesome content goes here */
